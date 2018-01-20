869268900f21f14c96c1dd1cadf91153159e80dc
JellyWX <judewrs@gmail.com>
2018-01-20 13:07:10 +0000

    updated globalvars to support the new json files

--- RemindMe/globalvars.py
+++ RemindMe/globalvars.py
@@ -6,32 +6,42 @@ intervals = []
 todos = {}
 
 try:
-  with open('DATA/calendar.csv','r') as f:
-    cal = f.read()
-    cal = cal.strip()
-    cal = ''.join(cal)
-    cal = cal.split(';')
-
-    for item in cal:
-      item = item.split(',')
-      calendar.append(item)
+  with open('DATA/calendar.json', 'r') as f:
+    calendar = json.load(f)
 
 except FileNotFoundError:
-  print('no calendar file found. not loading any reminders')
+  try:
+    with open('DATA/calendar.csv', 'r') as f:
+      cal = f.read()
+      cal = cal.strip()
+      cal = ''.join(cal)
+      cal = cal.split(';')
+
+      for item in cal:
+        item = item.split(',')
+        calendar.append(item)
+
+  except FileNotFoundError:
+    print('no calendar file found. not loading any reminders')
 
 try:
-  with open('DATA/intervals.csv','r') as f:
-    inv = f.read()
-    inv = inv.strip()
-    inv = ''.join(inv)
-    inv = inv.split(';')
-
-    for item in inv:
-      item = item.split(',')
-      intervals.append(item)
+  with open('DATA/intervals.json', 'r') as f:
+    intervals = json.load(f)
 
 except FileNotFoundError:
-  print('no interval file found. not loading any intervals')
+  try:
+    with open('DATA/intervals.csv','r') as f:
+      inv = f.read()
+      inv = inv.strip()
+      inv = ''.join(inv)
+      inv = inv.split(';')
+
+      for item in inv:
+        item = item.split(',')
+        intervals.append(item)
+
+  except FileNotFoundError:
+    print('no interval file found. not loading any intervals')
 
 try:
   with open('DATA/todos.json','r') as f:
