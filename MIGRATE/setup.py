023c5ad7ae5c5290c645966ffee325f57a31ec95
JellyWX <judewrs@gmail.com>
2018-03-10 20:53:27 +0000

    working on a setup file to help people run the bot themselves

---
+++ MIGRATE/setup.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+
+try:
+    os.mkdir('../DATA')
+except FileExistsError:
+    pass
+
+files = ['autoclears', 'blacklist', 'join_messages', 'leave_messages', 'log', 'prefix', 'restrictions', 'spamfilter', 'tags', 'timezones', 'todos']
+contents = ['[]', '[]', '{}', '{}', '{}', '{}', '{}', '[]', '{}', '{}', '{}']
+
+for fn, content in zip(files, contents):
+    f = open('../DATA' + fn + '.json', 'w')
+    f.write(content)
+    f.close()
+
+connection = sqlite3.connect('../DATA/calendar.db')
+cursor = connection.cursor()
+
+command = '''CREATE TABLE reminders (
+interval INTEGER,
+time INTEGER,
+message VARCHAR(400),
+channel INTEGER,
+deleted BOOLEAN
+);'''
+
+cursor.execute(command)
+connection.commit()
+connection.close()
