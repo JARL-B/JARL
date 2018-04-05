c765dbb8efd7e4e9bcca59fcd668f70de1086f09
jude-LafiteIII <judewrs@gmail.com>
2018-04-05 16:10:20 +0100

    made the help menu much nicer

--- Bot/get_help.py
+++ Bot/get_help.py
@@ -5,14 +5,7 @@ import asyncio
 
 
 async def get_help(message, client):
-    em = discord.Embed(title='**HELP**',description=
-    '''
-__Key Commands__
-    > `mbprefix <string>` - change the prefix from $ to anything less than 5 characters. This variable is stored on a per-server level. This command does not use a prefix!
-    > `$help` - get this page.
-    > `$info` - get info on the bot.
-    > `$blacklist [channel-name]` - block or unblock a channel from sending commands. If the bot has sufficient rights, it will also remove any commands in blacklisted channels.
-
+    em = discord.Embed(title='Help', description='''
 __Reminder Commands__
     > `$del` - delete reminders and intervals on your server.
     > `$remind [user/channel] <time-to-reminder> <message>` - set up a reminder. Takes times in the format of [num][s/m/h/d], for example 10s for 10 seconds or 2s10m for 2 seconds 10 minutes. An exact time can be provided as `day`/`month`/`year`-`hour`:`minute`:`second`.
@@ -20,31 +13,28 @@ __Reminder Commands__
     > `$todo` - TODO list related commands. Use `$todo help` for more information.
     > `$todos` - same as `$todo` but for server-wide task management.
     > `$timezone` - set your server's timezone, for easier date-based reminders
-    '''
-    )
-
-    em2 = discord.Embed(title='**HELP**', description='''
 __TheManagement Commands__
     > `$autoclear [time/s] [channels]` - enables/disables autoclearing, where messages sent to the channel (default your channel) will be automatically deleted after time (default 10 seconds)
     > `$clear <user mentions>` - clears messages made by a user/s
     > `$restrict [role mentions]` - add/remove roles from being allowed to send channel reminders and intervals.
-    > `$tag` - Aliasing commands. Use `$tag help` for more information
+    > `$tag` - Aliasing commands. Use `$tag help` for more information.
+    > `$blacklist [channel-name]` - block or unblock a channel from sending commands.
 
 __Other Commands__
     > `$donate` - view information about donations.
+    > `mbprefix <string>` - change the prefix from $. This command does not use a prefix!
+    > `$info` - get info on the bot.
 
     > a word surrounded by `<` `>` is a required argument
     > a word surrounded by `[` `]` is an optional argument
     > do not type the brackets when you type out the command! For example, `mbprefix !`, not `mbprefix <!>`
 
-*Do you have a place I can go to get more assistance?*
-    Please join our Discord server :)
+    Please join our Discord server if you need more help
 
     https://discord.gg/WQVaYmT
     '''
     )
 
     await message.channel.send(embed=em)
-    await message.channel.send(embed=em2)
 
     await message.add_reaction('📬')
