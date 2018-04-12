ebd895c781046acf53580af58afd1622772fd86b
jude-LafiteIII <judewrs@gmail.com>
2018-04-12 20:01:41 +0100

    todo stuff (untested)

--- Bot2/main.py
+++ Bot2/main.py
@@ -13,6 +13,7 @@ import sys
 import os
 import configparser
 import sqlite3
+import json
 
 class BotClient(discord.Client):
     def __init__(self, *args, **kwargs):
@@ -85,6 +86,13 @@ class BotClient(discord.Client):
             self.reminders.append(Reminder(dictv=dict(reminder))) #place each in the list
 
         self.reminders.sort(key=lambda x: x.time)
+
+        try:
+            with open('DATA/todos.json', 'r') as f:
+                self.todos = json.load(f)
+        except FileNotFoundError:
+            print('No todos file found')
+            self.todos = {}
 
         try:
             open('EXT/strings.py', 'r').close()
@@ -467,7 +475,69 @@ class BotClient(discord.Client):
 
 
     async def todo(self, message, stripped, server):
-        pass
+        if 'todos' in message.content.split(' ')[0]:
+            if isinstance(message.channel, discord.DMChannel):
+                await message.channel.send('Please use `$todo` for your personal TODO list. `$todos` is only for server use.')
+                return
+
+            location = message.guild.id
+            name = message.guild.name
+            command = 'todos'
+        else:
+            location = message.author.id
+            name = message.author.name
+            command = 'todo'
+
+
+        if location not in self.todos.keys():
+            self.todos[location] = []
+
+        splits = stripped.split(' ')
+
+        todo = self.todos[location]
+
+        if len(splits) == 1:
+            msg = ['\n{}: {}'.format(i+1, todo[i]) for i in range(len(todo))]
+            if len(msg) == 0:
+                msg.append('*Do `${0} add <message>` to add an item to your TODO, or type `${0} help` for more commands!*'.format(command))
+            await message.channel.send(embed=Embed(title='{}\'s TODO'.format(name), description=''.join(msg)))
+
+        elif len(splits) > 2:
+            if splits[1] in ['add', 'a']:
+                a = ' '.join(splits[2:])
+                if len(a) > 80:
+                    await message.channel.send('Sorry, but TODO message sizes are limited to 80 characters. Keep it concise :)')
+                    return
+
+                elif len(''.join(todo)) > 800:
+                    await message.channel.send('Sorry, but TODO lists are capped at 800 characters. Maybe, get some things done?')
+                    return
+
+                self.todos[location].append(a)
+                await message.channel.send('Added \'{}\' to todo!'.format(a))
+
+            elif splits[1] in ['remove', 'r']:
+                try:
+                    a = self.todos[location].pop(int(splits[2])-1)
+                    await message.channel.send('Removed \'{}\' from todo!'.format(a))
+
+                except ValueError:
+                    await message.channel.send('Removal item must be a number. View the numbered TODOs using `${}`'.format(command))
+                except IndexError:
+                    await message.channel.send('Couldn\'t find item by that number. Are you in the correct todo list?')
+
+            else:
+                await message.channel.send('To use the TODO commands, do `${0} add <message>`, `${0} remove <number>`, `${0} clear` and `${0}` to add to, remove from, clear or view your todo list.'.format(command))
+
+        elif splits[1] in ['remove*', 'r*', 'clear', 'clr']:
+            self.todos[location] = []
+            await message.channel.send('Cleared todo list!')
+
+        else:
+            await message.channel.send('To use the TODO commands, do `${0} add <message>`, `${0} remove <number>`, `${0} clear` and `${0}` to add to, remove from, clear or view your todo list.'.format(command))
+
+        with open('DATA/todos.json', 'w') as f:
+            json.dump(self.todos, f)
 
 
     async def delete(self, message, stripped, server):
