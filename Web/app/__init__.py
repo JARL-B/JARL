d6d7fd5f77eb4b6a74a89e0128a49064bc10cc54
root <root@jellywx.co.uk>
2018-05-30 09:55:42 +0000

    patch to the imports

--- Web/app/__init__.py
+++ Web/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from flask_dance import make_discord_blueprint, discord
+from flask_dance.contrib.discord import make_discord_blueprint, discord
 
 app = Flask(__name__)
 app.config.from_object(Config)
