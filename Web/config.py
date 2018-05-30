c5f6a22ce892af349f371f75fe1613b4aacee747
jude-LafiteIII <judewrs@gmail.com>
2018-05-30 19:27:33 +0100

    key loading

--- Web/config.py
+++ Web/config.py
@@ -1,7 +1,14 @@
 import os
+import json
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'very secret key'
 
-    DISCORD_OAUTH_CLIENT_ID = os.environ.get('DISCORD_OAUTH_CLIENT_ID') or '440052776702312450'
-    DISCORD_OAUTH_CLIENT_SECRET = os.environ.get('DISCORD_OAUTH_CLIENT_SECRET') or 'FjMSodOATz4FoXByylp6QS0lWQLeYKWa'
+    with open('/root/keys.json', 'r') as f:
+        client_id = json.load(f)['DISCORD_OAUTH_CLIENT_ID']
+        client_secret = json.load(f)['DISCORD_OAUTH_CLIENT_SECRET']
+        secret = json.load(f)['SECRET']
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secret
+
+    DISCORD_OAUTH_CLIENT_ID = os.environ.get('DISCORD_OAUTH_CLIENT_ID') or client_id
+    DISCORD_OAUTH_CLIENT_SECRET = os.environ.get('DISCORD_OAUTH_CLIENT_SECRET') or client_secret
