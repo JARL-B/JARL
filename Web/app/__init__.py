5c8722c315ba6bfe97bd0408775aba32096417ee
jude-LafiteIII <judewrs@gmail.com>
2018-06-23 09:54:51 +0100

    redirect url

--- Web/app/__init__.py
+++ Web/app/__init__.py
@@ -4,7 +4,7 @@ from flask_dance.contrib.discord import make_discord_blueprint, discord
 
 app = Flask(__name__)
 app.config.from_object(Config)
-discord_blueprint = make_discord_blueprint(scope=['identify', 'guilds'])
+discord_blueprint = make_discord_blueprint(scope=['identify', 'guilds'], redirect_url='dashboard')
 app.register_blueprint(discord_blueprint, url_prefix='/login')
 
 from app import routes
